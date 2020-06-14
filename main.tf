639cf847ad2074eedcdba01b167d4127b712494a
Alan Tong <45914772+Soupcodes@users.noreply.github.com>
2020-06-14 13:40:27 +0100

    added a variables file to be able to extract potentially sensitive details

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
 provider "aws" {
   profile = "default"
-  region  = "eu-west-2"
+  region  = var.region
 }
 
-resource "aws_instance" "alan_ec2" {
+resource "aws_instance" "test_ec2" {
   ami           = "ami-032598fcc7e9d1c7a"
   instance_type = "t2.micro"
   # Adding a `depends on` argument means the s3 bucket will be created before the ec2 instance starts even though they can be created in parallel 
   depends_on = [aws_s3_bucket.tf_s3_bucket]
+
+  # provisioner "local-exec" {
+  #   command = "echo ${aws_instance.test_ec2.public_ip} > myip.txt"
+  # }
 }
 
 resource "aws_eip" "tf_elastic_ip" {
   vpc      = true
-  instance = aws_instance.alan_ec2.id
+  instance = aws_instance.test_ec2.id
 }
 
 resource "aws_s3_bucket" "tf_s3_bucket" {
