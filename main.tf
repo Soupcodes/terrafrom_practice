fa87f8bf452149c4d4b20ac932471e18395fce01
Alan Tong <45914772+Soupcodes@users.noreply.github.com>
2020-06-14 18:44:17 +0100

    added new methods of variable management and security group

--- main.tf
+++ main.tf
@@ -1,25 +1,64 @@
 provider "aws" {
+  version = "~> 2.66"
+
   profile = "default"
-  region  = var.region
+  region  = var.region.default
 }
 
-resource "aws_instance" "test_ec2" {
-  ami           = "ami-032598fcc7e9d1c7a"
+provider "aws" {
+  alias = "virginia"
+  region = var.region.virginia
+}
+
+resource "aws_instance" "london_ec2" {
+  ami           = var.amis[var.region.default]
   instance_type = "t2.micro"
   # Adding a `depends on` argument means the s3 bucket will be created before the ec2 instance starts even though they can be created in parallel 
   depends_on = [aws_s3_bucket.tf_s3_bucket]
 
   # provisioner "local-exec" {
-  #   command = "echo ${aws_instance.test_ec2.public_ip} > myip.txt"
+  #   command = "echo ${aws_instance.london_ec2.public_ip} > myip.txt"
   # }
+}
+
+resource "aws_instance" "virginia_ec2" {
+  provider = aws.virginia
+
+  ami = var.amis[var.region.virginia]
+  instance_type = "t2.micro"
 }
 
 resource "aws_eip" "tf_elastic_ip" {
   vpc      = true
-  instance = aws_instance.test_ec2.id
+  instance = aws_instance.london_ec2.id
 }
 
 resource "aws_s3_bucket" "tf_s3_bucket" {
   bucket = "soup-tf-test"
   acl    = "private"
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH into EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
+# The .tfvars file can be used to set default values but those variables can only be used at the child module level if defined
+# The .tf variables file can be used to DECLARE variables that will exist in the configuration at this particular module level.
+# Without the .tf module at the correct level, your main.tf file will not be able to reference the relevant variables
+
 variable "region" {
-    default = "eu-west-2"
+    type = map(string)
+    description = "Region names for AWS"
+    # Note: Adding a `default` argument reassigns the variable declared in the .tfvars root module. eg:
+    # default = {}
+}
+
+variable "amis" {
+    type = map(string)
+    description = "Default free tier AMIs for each AWS region"
+}
+
+output "ip" {
+    value = aws_eip.tf_elastic_ip.public_ip
 }
